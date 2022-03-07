d9d30052c43ab1a27ead1636422b1a6e741a988e
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-03-07 20:58:26 +0530

    final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,12 @@ import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import Menu from './Modal/images/menu.svg';
+import { useState } from "react";
+import "./styles.css";
+import Menu from './Modal/images/user.png';
+import heart from './Modal/images/heart.png';
+import search from './Modal/images/search.png';
+
 
 const Container = styled.div`
   padding: 0rem 0;
@@ -65,7 +70,8 @@ const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
+  justify-content: flex-center;
+  gap:20px;
 `;
 
 const MenuItem = styled.div`
@@ -79,8 +85,8 @@ const MenuItem = styled.div`
 `;
 
 const Image = styled.img`
-  width:25px;
-  height:25px
+  width:18px;
+  height:18px
 `;
 
 const MenuItems = styled.h3`
@@ -93,6 +99,11 @@ const MenuItems = styled.h3`
 const Navbar = () => {
   const quantity = useSelector(state => state.cart.quantity)
   let user = useSelector((state) => state.user.currentUser);
+  const [dropdownState, setDropdownState] = useState(false);
+
+  const handleDropdownClick = () => {
+    setDropdownState(!dropdownState);
+  };
 
   const signOut = () => {
     localStorage.removeItem('persist:root')
@@ -109,19 +120,28 @@ const Navbar = () => {
         </Left>
 
         <Center>
-          <MenuItems>MEN</MenuItems>
-          <MenuItems>WOMAN</MenuItems>
-          <MenuItems>CHILDREN</MenuItems>
+          <Link to={`/products/man`}>
+            <MenuItems>MEN</MenuItems>
+          </Link>
+          <Link to={`/products/woman`}>
+            <MenuItems>WOMAN</MenuItems>
+          </Link>
+          <Link to={`/products/children`}>
+            <MenuItems>CHILDREN</MenuItems>
+          </Link>
+          <Link to={`/products/new arrivals`}>
           <MenuItems>NEW ARRIVALS</MenuItems>
+          </Link>
+          <Link to={`/products/clearance`}>
           <MenuItems>CLEARANCE</MenuItems>
+          </Link>
         </Center>
 
         <Right>
-          {
+          {/* {
             user ? (
               <>
-              <MenuItem onClick={() => signOut()}><b>Sign out</b></MenuItem>
-              <Link to="/success"> <MenuItem><b>Orders</b></MenuItem></Link>
+                <MenuItem onClick={() => signOut()}><b>Sign out</b></MenuItem>
               </>
             ) : (
               <>
@@ -129,16 +149,71 @@ const Navbar = () => {
                 <Link to="/register"><MenuItem><b>Register</b></MenuItem></Link>
               </>
             )
-          }
+          } */}
 
 
           <MenuItem>
-            <Link to="/cart">
-              <Badge badgeContent={quantity} color="primary">
-                <ShoppingCartIcon />
-              </Badge>
+            <Link to="/cart" >
+              <div style={{ textAlign: 'center' }}>
+                <Badge badgeContent={quantity} color="primary" style={{ marginTop: '-7px', marginBottom: '5px' }}>
+                  <ShoppingCartIcon />
+                </Badge>
+                <div className="profile-title-2">Cart</div>
+              </div>
             </Link>
           </MenuItem>
+
+          <div className={`dropdown`}>
+
+            <div style={{ textAlign: 'center' }} onClick={handleDropdownClick}>
+              <Image src={Menu} alt="" className="dropdown-btn" />
+              <div className="profile-title">Profile</div>
+            </div>
+
+            <div className={`dropdown-items ${dropdownState ? "isVisible" : "isHidden"}`}>
+              {user ? (
+                <>
+                  <div className="dropdown-item">
+                    <div className="dropdown__link" onClick={() => signOut()}> Sign out</div>
+                  </div>
+
+                  <div className="dropdown-item">
+                    <Link to="/success"> <div className="dropdown__link"> Orders </div></Link>
+                  </div>
+
+                </>
+              ) : (
+                <>
+                  <div className="dropdown-item">
+                    <Link to="/login"><div className="dropdown__link"> Login </div></Link>
+                  </div>
+                  <div className="dropdown-item">
+                    <Link to="/register"> <div className="dropdown__link"> Register </div></Link>
+                  </div>
+
+                </>
+              )
+              }
+              <div className="dropdown-item">
+                <Link to=""> <div className="dropdown__link"> Gift Card </div></Link>
+              </div>
+
+
+              <div className="dropdown-item">
+                <Link to=""> <div className="dropdown__link"> Contact us</div></Link>
+              </div>
+            </div>
+          </div>
+
+          <div style={{ textAlign: 'center' }}>
+            <Image src={heart} alt="" className="dropdown-btn" />
+            <div className="profile-title">Wishlist</div>
+          </div>
+
+          <div style={{ textAlign: 'center' }}>
+            <Image src={search} alt="" className="dropdown-btn" />
+            <div className="profile-title">Search</div>
+          </div>
 
         </Right>
       </Wrapper>
