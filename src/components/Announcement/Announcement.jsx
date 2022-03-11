11af93f80fafa09d8d81b474557c74e994b39bad
Shihab Shaikh <88572629+shihab-fw11-297@users.noreply.github.com>
2022-03-11 20:30:16 +0530

    Delete src/components directory

--- src/components/Announcement/Announcement.jsx
+++
@@ -1,7 +0,0 @@
-import { Container } from './Announcement.styled';
-
-const Announcement = () => {
-  return <Container>50-70% Off Sitewide Including New Arrivals! "exclusions apply</Container>;
-};
-
-export default Announcement;
